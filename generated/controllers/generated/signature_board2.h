768cf5cdfdbf097d200a9556399e5bf6cbce969c
EDMaus <maurice.post@hotmail.com>
2026-03-22 17:13:04 +0100

    Remove example board targets and generated files

--- generated/controllers/generated/signature_board2.h
+++
@@ -1,6 +0,0 @@
-//
-// CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 1297417365
-#define TS_SIGNATURE "rusEFI main.2026.03.22.board2.1297417365"
--- generated/controllers/generated/signature_xyz.h
+++
@@ -1,6 +0,0 @@
-//
-// CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 220064438
-#define TS_SIGNATURE "rusEFI main.2026.03.22.xyz.220064438"
