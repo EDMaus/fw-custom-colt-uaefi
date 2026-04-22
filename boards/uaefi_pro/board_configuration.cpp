5e1100fd35f447fc6fe59d846548a6538315d9d7
EDMaus <maurice.post@hotmail.com>
2026-04-22 15:02:01 +0200

    Send Colt dash RPM explicitly on CAN1

--- boards/uaefi_pro/board_configuration.cpp
+++ boards/uaefi_pro/board_configuration.cpp
@@ -254,7 +254,7 @@ void boardUpdateDash(CanCycle cycle) {
 
     const uint16_t rawRpm = encodeColtDashRpm(Sensor::getOrZero(SensorType::Rpm));
 
-    CanTxMessage msg(CanCategory::NBC, 0x308, 8);
+    CanTxMessage msg(CanCategory::NBC, 0x308, 8, 0);
 
 
     msg[0] = 0x00;
