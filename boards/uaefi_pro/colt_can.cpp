2aa4812e403348c53ec05b63fb1caa13811526e3
EDMaus <maurice.post@hotmail.com>
2026-05-13 20:24:19 +0200

    Stop Colt 0x1E1 transmit to avoid SRS bus conflict

--- boards/uaefi_pro/colt_can.cpp
+++ boards/uaefi_pro/colt_can.cpp
@@ -48,18 +48,6 @@ static uint16_t encodeColtDashRpm(float rpm) {
 	return static_cast<uint16_t>(raw);
 }
 
-static void sendFrame1E1() {
-	CanTxMessage msg(CanCategory::NBC, 0x1E1, 8, COLT_CAN_BUS);
-	msg[0] = 0x00;
-	msg[1] = 0x00;
-	msg[2] = 0x00;
-	msg[3] = 0x00;
-	msg[4] = 0x00;
-	msg[5] = 0x00;
-	msg[6] = 0x00;
-	msg[7] = 0x00;
-}
-
 static void sendFrame210() {
 	CanTxMessage msg(CanCategory::NBC, 0x210, 8, COLT_CAN_BUS);
 	msg[0] = 0x00;
@@ -201,7 +189,6 @@ void processColtCanTx(CanCycle cycle) {
 
 	if (cycle.isInterval(CI::_20ms)) {
 		g_ignitionOn20msTicks++;
-		sendFrame1E1();
 		sendFrame210();
 		sendFrame212();
 		sendFrame308();
