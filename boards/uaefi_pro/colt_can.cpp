1cbff5f282591b8a0e125fb18b40b8371d0cb46a
EDMaus <maurice.post@hotmail.com>
2026-05-15 01:17:26 +0200

    Disable Colt 0x1E1 TX to remove dual-source SRS flicker

--- boards/uaefi_pro/colt_can.cpp
+++ boards/uaefi_pro/colt_can.cpp
@@ -11,7 +11,6 @@ namespace {
 static constexpr size_t COLT_CAN_BUS = 0;
 static constexpr uint32_t COLT_MIL_BULB_CHECK_20MS_TICKS = 200; // 4 seconds
 static constexpr uint32_t COLT_MIL_SELF_CHECK_SETTLE_20MS_TICKS = 250; // 5 seconds total
-static constexpr uint32_t COLT_SRS_CLEAR_START_20MS_TICKS = 335; // ~6.7 seconds
 
 struct ColtRuntimeState {
 	bool brakePressed = false;
@@ -47,18 +46,6 @@ static uint16_t encodeColtDashRpm(float rpm) {
 	}
 
 	return static_cast<uint16_t>(raw);
-}
-
-static void sendFrame1E1Clear() {
-	CanTxMessage msg(CanCategory::NBC, 0x1E1, 8, COLT_CAN_BUS);
-	msg[0] = 0x00;
-	msg[1] = 0x00;
-	msg[2] = 0x00;
-	msg[3] = 0x00;
-	msg[4] = 0x00;
-	msg[5] = 0x00;
-	msg[6] = 0x00;
-	msg[7] = 0x00;
 }
 
 static void sendFrame210() {
@@ -212,10 +199,6 @@ void processColtCanTx(CanCycle cycle) {
 
 	if (cycle.isInterval(CI::_20ms)) {
 		g_ignitionOn20msTicks++;
-		const bool shouldHoldSrsClear = isEngineRunning() || (g_ignitionOn20msTicks >= COLT_SRS_CLEAR_START_20MS_TICKS);
-		if (shouldHoldSrsClear) {
-			sendFrame1E1Clear();
-		}
 		sendFrame210();
 		sendFrame212();
 		sendFrame308();
