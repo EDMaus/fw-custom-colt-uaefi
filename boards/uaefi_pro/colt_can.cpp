748033ab1bc3534b514a50c3ff5c53a27b8585b4
EDMaus <maurice.post@hotmail.com>
2026-05-14 17:55:04 +0200

    Mirror OEM 0x210 alternating run states

--- boards/uaefi_pro/colt_can.cpp
+++ boards/uaefi_pro/colt_can.cpp
@@ -21,6 +21,7 @@ struct ColtRuntimeState {
 
 static ColtRuntimeState g_coltCanState;
 static uint32_t g_ignitionOn20msTicks = 0;
+static bool g_frame210Toggle = false;
 
 static int getCurrentRpm() {
 	return static_cast<int>(Sensor::getOrZero(SensorType::Rpm));
@@ -55,9 +56,20 @@ static void sendFrame210() {
 	msg[2] = (!isEngineRunning() && g_ignitionOn20msTicks <= COLT_MIL_BULB_CHECK_20MS_TICKS) ? 0x01 : 0x00;
 	msg[3] = 0x00;
 	msg[4] = 0x00;
-	msg[5] = isEngineRunning() ? 0x80 : 0x00;
+	msg[5] = 0x00;
 	msg[6] = 0x00;
 	msg[7] = 0xFF;
+
+	// OEM alternates two 0x210 states (byte3=0x40 and byte5=0x80). Mirror that pattern.
+	if (isEngineRunning()) {
+		if (g_frame210Toggle) {
+			msg[3] = 0x40;
+			msg[5] = 0x00;
+		} else {
+			msg[3] = 0x00;
+			msg[5] = 0x80;
+		}
+	}
 }
 
 static void sendFrame212() {
@@ -171,6 +183,7 @@ void initColtCan() {
 #if !defined(EFI_BOOTLOADER) && EFI_CAN_SUPPORT
 	g_coltCanState = {};
 	g_ignitionOn20msTicks = 0;
+	g_frame210Toggle = false;
 #endif
 }
 
@@ -191,6 +204,7 @@ void processColtCanTx(CanCycle cycle) {
 
 	if (cycle.isInterval(CI::_20ms)) {
 		g_ignitionOn20msTicks++;
+		g_frame210Toggle = !g_frame210Toggle;
 		sendFrame210();
 		sendFrame212();
 		sendFrame308();
