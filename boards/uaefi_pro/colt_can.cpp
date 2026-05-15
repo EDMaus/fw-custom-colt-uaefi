916703c618c8c759e016be325e558e14df1d5f7f
EDMaus <49249233+EDMaus@users.noreply.github.com>
2026-05-15 14:51:14 +0200

    Add Colt 0x101 and 0x111 key-on init burst

--- boards/uaefi_pro/colt_can.cpp
+++ boards/uaefi_pro/colt_can.cpp
@@ -11,7 +11,7 @@ namespace {
 static constexpr size_t COLT_CAN_BUS = 0;
 static constexpr uint32_t COLT_MIL_BULB_CHECK_20MS_TICKS = 200; // 4 seconds
 static constexpr uint32_t COLT_MIL_SELF_CHECK_SETTLE_20MS_TICKS = 250; // 5 seconds total
-static constexpr uint32_t COLT_SRS_CLEAR_ENABLE_20MS_TICKS = 650; // ~13 seconds
+static constexpr uint32_t COLT_KEY_ON_INIT_BURST_20MS_TICKS = 10;
 
 struct ColtRuntimeState {
 	bool brakePressed = false;
@@ -49,9 +49,21 @@ static uint16_t encodeColtDashRpm(float rpm) {
 	return static_cast<uint16_t>(raw);
 }
 
-static void sendFrame1E1Clear() {
-	CanTxMessage msg(CanCategory::NBC, 0x1E1, 8, COLT_CAN_BUS);
+static void sendFrame101InitPrimary() {
+	CanTxMessage msg(CanCategory::NBC, 0x101, 8, COLT_CAN_BUS);
 	msg[0] = 0x00;
+	msg[1] = 0x11;
+	msg[2] = 0x51;
+	msg[3] = 0xEE;
+	msg[4] = 0xAE;
+	msg[5] = 0x00;
+	msg[6] = 0x00;
+	msg[7] = 0x22;
+}
+
+static void sendFrame101InitSecondary() {
+	CanTxMessage msg(CanCategory::NBC, 0x101, 8, COLT_CAN_BUS);
+	msg[0] = 0xFF;
 	msg[1] = 0x00;
 	msg[2] = 0x00;
 	msg[3] = 0x00;
@@ -59,6 +71,25 @@ static void sendFrame1E1Clear() {
 	msg[5] = 0x00;
 	msg[6] = 0x00;
 	msg[7] = 0x00;
+}
+
+static void sendFrame111Init() {
+	CanTxMessage msg(CanCategory::NBC, 0x111, 8, COLT_CAN_BUS);
+	msg[0] = 0x00;
+	msg[1] = 0xF8;
+	msg[2] = 0x89;
+	msg[3] = 0x42;
+	msg[4] = 0x00;
+	msg[5] = 0x00;
+	msg[6] = 0x00;
+	msg[7] = 0x00;
+}
+
+static void sendKeyOnInitBurst() {
+	sendFrame101InitPrimary();
+	sendFrame111Init();
+	sendFrame101InitSecondary();
+	sendFrame111Init();
 }
 
 static void sendFrame210() {
@@ -221,6 +252,9 @@ void processColtCanTx(CanCycle cycle) {
 
 	if (cycle.isInterval(CI::_20ms)) {
 		g_ignitionOn20msTicks++;
+		if (g_ignitionOn20msTicks <= COLT_KEY_ON_INIT_BURST_20MS_TICKS) {
+			sendKeyOnInitBurst();
+		}
 		sendFrame210();
 		sendFrame212();
 		sendFrame308();
@@ -260,13 +294,6 @@ void processColtCanRx(uint32_t id, const uint8_t* data, uint8_t dlc) {
 		case 0x443:
 			if (dlc > 0) {
 				g_coltCanState.acRequest = (data[0] & 0x01) != 0;
-			}
-			break;
-
-		case 0x1E1:
-			// After OEM-like self-check window, immediately clear any 0x81 assertion.
-			if (dlc > 0 && data[0] == 0x81 && g_ignitionOn20msTicks >= COLT_SRS_CLEAR_ENABLE_20MS_TICKS) {
-				sendFrame1E1Clear();
 			}
 			break;
 
