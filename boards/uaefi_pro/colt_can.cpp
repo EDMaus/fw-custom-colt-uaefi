a07cb9613ddae3f9a08d4a13c9e375939398e27f
EDMaus <49249233+EDMaus@users.noreply.github.com>
2026-05-15 14:57:31 +0200

    Extend Colt key-on init burst to 500ms

--- boards/uaefi_pro/colt_can.cpp
+++ boards/uaefi_pro/colt_can.cpp
@@ -11,7 +11,7 @@ namespace {
 static constexpr size_t COLT_CAN_BUS = 0;
 static constexpr uint32_t COLT_MIL_BULB_CHECK_20MS_TICKS = 200; // 4 seconds
 static constexpr uint32_t COLT_MIL_SELF_CHECK_SETTLE_20MS_TICKS = 250; // 5 seconds total
-static constexpr uint32_t COLT_KEY_ON_INIT_BURST_20MS_TICKS = 10;
+static constexpr uint32_t COLT_KEY_ON_INIT_BURST_20MS_TICKS = 25;
 
 struct ColtRuntimeState {
 	bool brakePressed = false;
