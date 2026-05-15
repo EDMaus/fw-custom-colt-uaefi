59d31e44fb4d3640a934fd0b0830b739535b5190
EDMaus <maurice.post@hotmail.com>
2026-05-15 02:53:25 +0200

    Reassert Colt 0x1E1 clear when 0x81 seen on bus

--- boards/uaefi_pro/colt_can.cpp
+++ boards/uaefi_pro/colt_can.cpp
@@ -11,6 +11,7 @@ namespace {
 static constexpr size_t COLT_CAN_BUS = 0;
 static constexpr uint32_t COLT_MIL_BULB_CHECK_20MS_TICKS = 200; // 4 seconds
 static constexpr uint32_t COLT_MIL_SELF_CHECK_SETTLE_20MS_TICKS = 250; // 5 seconds total
+static constexpr uint32_t COLT_SRS_CLEAR_START_20MS_TICKS = 335; // ~6.7 seconds
 
 struct ColtRuntimeState {
 	bool brakePressed = false;
@@ -46,6 +47,18 @@ static uint16_t encodeColtDashRpm(float rpm) {
 	}
 
 	return static_cast<uint16_t>(raw);
+}
+
+static void sendFrame1E1Clear() {
+	CanTxMessage msg(CanCategory::NBC, 0x1E1, 8, COLT_CAN_BUS);
+	msg[0] = 0x00;
+	msg[1] = 0x00;
+	msg[2] = 0x00;
+	msg[3] = 0x00;
+	msg[4] = 0x00;
+	msg[5] = 0x00;
+	msg[6] = 0x00;
+	msg[7] = 0x00;
 }
 
 static void sendFrame210() {
@@ -199,6 +212,10 @@ void processColtCanTx(CanCycle cycle) {
 
 	if (cycle.isInterval(CI::_20ms)) {
 		g_ignitionOn20msTicks++;
+		const bool shouldHoldSrsClear = isEngineRunning() || (g_ignitionOn20msTicks >= COLT_SRS_CLEAR_START_20MS_TICKS);
+		if (shouldHoldSrsClear) {
+			sendFrame1E1Clear();
+		}
 		sendFrame210();
 		sendFrame212();
 		sendFrame308();
@@ -238,6 +255,13 @@ void processColtCanRx(uint32_t id, const uint8_t* data, uint8_t dlc) {
 		case 0x443:
 			if (dlc > 0) {
 				g_coltCanState.acRequest = (data[0] & 0x01) != 0;
+			}
+			break;
+
+		case 0x1E1:
+			// If another module asserts "81" status, immediately re-assert clear.
+			if (dlc > 0 && data[0] == 0x81 && isIgnitionOn()) {
+				sendFrame1E1Clear();
 			}
 			break;
 
