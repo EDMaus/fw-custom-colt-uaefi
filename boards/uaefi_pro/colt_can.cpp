f6882abeafb6f94b42cd04b2c374a3e190f3f54a
EDMaus <49249233+EDMaus@users.noreply.github.com>
2026-05-15 18:45:45 +0200

    Remove ineffective Colt 0x443 override

--- boards/uaefi_pro/colt_can.cpp
+++ boards/uaefi_pro/colt_can.cpp
@@ -156,18 +156,6 @@ static void sendFrame312() {
 	msg[7] = 0x8E;
 }
 
-static void sendFrame443() {
-	CanTxMessage msg(CanCategory::NBC, 0x443, 8, COLT_CAN_BUS);
-	msg[0] = 0x00;
-	msg[1] = 0x11;
-	msg[2] = 0x00;
-	msg[3] = 0x00;
-	msg[4] = 0x00;
-	msg[5] = 0x00;
-	msg[6] = 0x00;
-	msg[7] = 0x00;
-}
-
 static void sendFrame608() {
 	CanTxMessage msg(CanCategory::NBC, 0x608, 8, COLT_CAN_BUS);
 
@@ -224,7 +212,6 @@ void processColtCanTx(CanCycle cycle) {
 		sendFrame212();
 		sendFrame308();
 		sendFrame312();
-		sendFrame443();
 	}
 
 	if (cycle.isInterval(CI::_100ms)) {
