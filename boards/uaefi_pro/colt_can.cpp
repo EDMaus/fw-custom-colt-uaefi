dee6b23ae4e4f437dea612f3eb08d6ca36616e76
EDMaus <maurice.post@hotmail.com>
2026-07-15 01:12:07 +0200

    Use hot CLT state for Colt 0x416 fan test

--- boards/uaefi_pro/colt_can.cpp
+++ boards/uaefi_pro/colt_can.cpp
@@ -304,7 +304,7 @@ static void sendFrame312() {
 
 static void sendFrame416() {
 	CanTxMessage msg(CanCategory::NBC, 0x416, 8, COLT_CAN_BUS);
-	msg[0] = isEngineRunning() ? 0x8E : 0x75;
+	msg[0] = (isEngineRunning() || isColtFanTestHot()) ? 0x8E : 0x75;
 	msg[1] = 0x00;
 	msg[2] = 0x00;
 	msg[3] = 0x00;
